9fbd7b7f0eb83c478d87e1128d2151806ecb97cd
kiramDZ <hilmi0@mhs.unsyiah.ac.id>
2024-02-09 14:40:26 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import Hero from './components/Hero';
+import Header from './components/Header';
+import Tabs from './components/Tabs';
+import Footer from './components/Footer';
+function App() {
+  return (
+  <div className='bg-[#f2f2f2] h-screen'>
+    <Header/>
+    <Hero/>
+    <Tabs/>
+    <Footer/>
+  </div>
+    );
+}
+
+export default App;
